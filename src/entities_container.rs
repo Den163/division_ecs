fa87da447f45d57b4bade86ed073e2ef9cead978
Den163 <abramov.denis163@gmail.com>
2022-02-06 12:11:50 +0400

    remoed all methods

--- src/entities_container.rs
+++ src/entities_container.rs
@@ -17,30 +17,18 @@ impl EntitiesContainer {
     }
 
     pub fn create_entity(&mut self) -> Entity {
-        for (i, e) in self.sparse_.iter_mut().enumerate() {
-            if !e.alive {
-                e.version += 1;
-                e.alive = true;
-
-                return  Entity {
-                    id: i as u32,
-                    version: e.version
-                };
-            }
-        }
-
-        panic!("Not implemented");
+        todo!();
     }
 
     pub fn try_destroy_entity(&mut self, _entity: Entity) -> Result<(), EntityRequestError> {
-        panic!("Not implemented");
+        todo!();
     }
 
     pub fn destroy_entity(&mut self, _entity: Entity) {
-        panic!("Not implemented");
+        todo!();
     }
 
     pub fn is_alive(&self, _entity: Entity) -> bool {
-        panic!("Not implemented");
+        todo!();
     }
 }
