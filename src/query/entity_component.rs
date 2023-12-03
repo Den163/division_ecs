ac8dbec511abef54764585513b3936c136202655
den163 <abramov.denis163@gmail.com>
2023-12-04 00:59:57 +0400

    Improved entities filtering in query

--- src/query/entity_component.rs
+++ src/query/entity_component.rs
@@ -13,7 +13,6 @@ pub type EntityComponentReadOnlyQuery<R> = EntityComponentQuery<ReadonlyAccess<R
 pub type EntityComponentWriteQuery<W> = EntityComponentQuery<WriteAccess<W>>;
 
 pub struct EntityComponentQuery<T: ComponentQueryAccess> {
-    filtered_entity_ids: Vec<u32>,
     entity_index_ranges: Vec<Range<usize>>,
     range_to_component_offsets: Vec<T::OffsetsTuple>,
     range_to_pages: Vec<*const ArchetypeDataPage>,
@@ -21,7 +20,7 @@ pub struct EntityComponentQuery<T: ComponentQueryAccess> {
 
 pub struct EntityComponentQueryIter<'a, T: ComponentQueryAccess> {
     store: &'a Store,
-    entity_ids: &'a [u32],
+    entities: &'a [Entity],
     entity_ranges: &'a [Range<usize>],
     range_component_offsets: &'a [T::OffsetsTuple],
     range_pages: &'a [*const ArchetypeDataPage],
@@ -33,13 +32,11 @@ pub struct EntityComponentQueryIter<'a, T: ComponentQueryAccess> {
 
 pub struct WithEntitiesIter<'a, T: ComponentQueryAccess> {
     source_iter: EntityComponentQueryIter<'a, T>,
-    entities_versions: *const u32,
 }
 
 impl<T: ComponentQueryAccess> EntityComponentQuery<T> {
     pub fn new() -> Self {
         Self {
-            filtered_entity_ids: Vec::new(),
             entity_index_ranges: Vec::new(),
             range_to_component_offsets: Vec::new(),
             range_to_pages: Vec::new(),
@@ -63,32 +60,24 @@ pub fn read_write<R: ComponentsTuple, W: ComponentsTuple>(
 impl Store {
     pub fn entity_component_query_iter<'a, 'b: 'a, T: ComponentQueryAccess>(
         &'a self,
-        entities: &[Entity],
+        entities: &'b [Entity],
         query: &'b mut EntityComponentQuery<T>,
     ) -> EntityComponentQueryIter<'a, T> {
         query.entity_index_ranges.clear();
         query.range_to_pages.clear();
         query.range_to_component_offsets.clear();
-        query.filtered_entity_ids.clear();
-
-        query.filtered_entity_ids.reserve(entities.len());
-
-        // TODO: This filter need to optimize strongly
-        query.filtered_entity_ids.extend(
-            entities
-                .iter()
-                .filter(|e| {
-                    self.get_entity_archetype(**e)
-                        .map(|arch| T::is_archetype_include_types(arch))
-                        .unwrap_or(false)
-                })
-                .map(|e| e.id),
-        );
 
         let mut range_start = 0;
-        while range_start < query.filtered_entity_ids.len() {
-            let id = query.filtered_entity_ids[range_start];
-            let range_page_index = unsafe { self.get_page_index_unchecked(id) as usize };
+        while range_start < entities.len() {
+            let range_entity = entities[range_start];
+
+            if self.is_valid_entity_for_query::<T>(range_entity) == false {
+                range_start += 1;
+                continue;
+            }
+
+            let range_page_index =
+                unsafe { self.get_page_index_unchecked(range_entity.id) as usize };
             let arch_index = self
                 .archetypes_container
                 .get_archetype_index_by_page(range_page_index);
@@ -101,9 +90,14 @@ impl Store {
             let comp_offsets = T::get_offsets(&arch);
 
             let mut range_end = range_start + 1;
-            while range_end < query.filtered_entity_ids.len() {
-                let id = query.filtered_entity_ids[range_end];
-                let page_index = unsafe { self.get_page_index_unchecked(id) as usize };
+            while range_end < entities.len() {
+                let inner_entity = entities[range_end];
+                if self.is_valid_entity_for_query::<T>(inner_entity) == false {
+                    break;
+                }
+
+                let page_index =
+                    unsafe { self.get_page_index_unchecked(inner_entity.id) as usize };
                 if range_page_index != page_index {
                     break;
                 }
@@ -120,7 +114,7 @@ impl Store {
 
         EntityComponentQueryIter {
             store: &self,
-            entity_ids: &query.filtered_entity_ids,
+            entities: &entities,
             entity_ranges: &query.entity_index_ranges,
             range_pages: &query.range_to_pages,
             range_component_offsets: &query.range_to_component_offsets,
@@ -128,6 +122,13 @@ impl Store {
             next_offset_from_range: 0,
             current_entity_index: 0,
         }
+    }
+
+    #[inline(always)]
+    fn is_valid_entity_for_query<T: ComponentQueryAccess>(&self, entity: Entity) -> bool {
+        self.get_entity_archetype(entity)
+            .map(|arch| T::is_archetype_include_types(arch))
+            .unwrap_or(false)
     }
 }
 
@@ -144,10 +145,10 @@ impl<'a, T: ComponentQueryAccess> Iterator for EntityComponentQueryIter<'a, T> {
         self.current_entity_index = current_range.start + self.next_offset_from_range;
 
         let (index_in_page, page, offsets) = unsafe {
-            let current_id = *self.entity_ids.get_unchecked(self.current_entity_index);
+            let current_entity = self.entities.get_unchecked(self.current_entity_index);
             let page = *self.range_pages.get_unchecked(self.next_range_index);
             (
-                self.store.get_index_in_page_unchecked(current_id),
+                self.store.get_index_in_page_unchecked(current_entity.id),
                 &*page,
                 self.range_component_offsets
                     .get_unchecked(self.next_range_index),
@@ -167,11 +168,7 @@ impl<'a, T: ComponentQueryAccess> Iterator for EntityComponentQueryIter<'a, T> {
 
 impl<'a, T: ComponentQueryAccess> EntityComponentQueryIter<'a, T> {
     pub fn with_entities(self) -> WithEntitiesIter<'a, T> {
-        let entities_versions = self.store.entities_container.entity_versions();
-        WithEntitiesIter {
-            source_iter: self,
-            entities_versions,
-        }
+        WithEntitiesIter { source_iter: self }
     }
 }
 
@@ -180,13 +177,12 @@ impl<'a, T: ComponentQueryAccess> Iterator for WithEntitiesIter<'a, T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.source_iter.next().map(|components| unsafe {
-            let id = *self
+            let entity = *self
                 .source_iter
-                .entity_ids
+                .entities
                 .get_unchecked(self.source_iter.current_entity_index);
-            let version = *self.entities_versions.add(id as usize);
 
-            (Entity { id, version }, components)
+            (entity, components)
         })
     }
 }
